978c6188a7d845aa097f8740775729968f21d4f1
necayoka <60106494+necayoka@users.noreply.github.com>
2020-01-21 15:38:12 +0100

    Add files via upload

---
+++ GitKalkulator/src/calc/KalkulatorDemo.java
@@ -0,0 +1,24 @@
+package calc;
+
+import java.util.Scanner;
+
+public class KalkulatorDemo {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Unesi prvi broj: ");
+		int broj1 = in.nextInt();
+		
+		System.out.print("Unesi drugi broj: ");
+		int broj2 = in.nextInt();
+		
+		int rezultat = broj1 + broj2;
+		System.out.println("Rezultat je: " + rezultat);
+		
+		in.close();
+		
+	}
+	
+}
